f7ee69e794e9f9a2479ff5f2c38a5aafeaa6acd6
Jephthah Orobia <115846010+jephthah-orobia@users.noreply.github.com>
2024-12-31 20:47:46 +0000

    format

--- src/main.cpp
+++ src/main.cpp
@@ -12,10 +12,11 @@
 
 using namespace std;
 
-void printDigits(short n){
+void printDigits(short n)
+{
   // only shorts from 10 to 99!
   cout << "The digits of " << n << " are: \033[32m"
-    << n / 10 << " " << n % 10 << "\033[0m" << endl;
+       << n / 10 << " " << n % 10 << "\033[0m" << endl;
 }
 
 int main()
@@ -23,32 +24,42 @@ int main()
   short n;
   char todoNext;
 
-  while(true){
-    if(cin.fail()){
+  while (true)
+  {
+    if (cin.fail())
+    {
       cin.clear();
       cin.ignore(numeric_limits<streamsize>::max(), '\n');
       cout << "\033[031mInvalid number. Please try again.\033[0m" << endl;
     }
     cout << "Enter a whole number from 10 to 99: ";
     cin >> noskipws >> n;
-    if(!cin.fail() && n >= 10 && n <= 99){
+    if (!cin.fail() && n >= 10 && n <= 99)
+    {
       cin.ignore(numeric_limits<streamsize>::max(), '\n');
       break;
-    } else {
-      if(!cin.fail()) cin.setstate(std::ios::failbit);
+    }
+    else
+    {
+      if (!cin.fail())
+        cin.setstate(std::ios::failbit);
     }
   }
 
-  cout << endl << "\t";
+  cout << endl
+       << "\t";
   printDigits(n);
 
-  cout << endl << "Enter \033[1m\033[33mr\033[0m or \033[1m\033[33mR\033[0m to restart app. Otherwise, app will exit: ";
+  cout << endl
+       << "Enter \033[1m\033[33mr\033[0m or \033[1m\033[33mR\033[0m to restart app. Otherwise, app will exit: ";
   cin >> todoNext;
 
-  if(todoNext == 'r' || todoNext == 'R'){
+  if (todoNext == 'r' || todoNext == 'R')
+  {
     system("clear");
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
     return main();
-  } else
+  }
+  else
     return EXIT_SUCCESS;
 }
